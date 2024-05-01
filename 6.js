e42494accc7d2c553cb442403352376967a5ad47
Oswaldo Valles <oswyvalles@gmail.com>
2024-04-30 22:20:34 -0600

    Agregado ejercicio 6. Hacer un programa que imprima todos los números naturales que hay desde el uno hasta un número que introducimos por teclado.

---
+++ 6.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Introduce un número: ', (numero) => {
+    numero = parseInt(numero);
+    for (let i = 1; i <= numero; i++) {
+        console.log(i);
+    }
+    rl.close();
+});
